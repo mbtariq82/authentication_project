1d652e3f5385354cfe3885d8b94f4d6dac36dd8e
Bilal <mbtariq82@gmail.com>
2026-07-18 19:19:33 +0100

    remove old frontend and changed .env path in config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-ENV_PATH = BASE_DIR / ".env"
+ENV_PATH = ".env"
 
 load_dotenv(ENV_PATH)
 
