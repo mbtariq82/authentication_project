8c2d3d4dc23db7b66fc4fe65099b2e381e7ef684
Bilal <mbtariq82@gmail.com>
2026-07-24 20:30:32 +0100

    forgot to add abstract method

--- backend/cache/abstract_user_cache.py
+++ backend/cache/abstract_user_cache.py
@@ -2,10 +2,11 @@ from abc import ABC, abstractmethod
 
 from schemas import UserResponse
 
-class AbstractUserCache:
+class AbstractUserCache(ABC):
     @abstractmethod
     async def get_by_id(self, user_id: int) -> UserResponse | None:
         raise NotImplementedError
 
+    @abstractmethod
     async def set(self, user: UserResponse) -> None:
         raise NotImplementedError
