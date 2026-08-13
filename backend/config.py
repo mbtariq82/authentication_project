e4ac7db77def5d5795ad1e37978352bd985d55bc
Bilal <mbtariq82@gmail.com>
2026-08-13 10:30:07 +0100

    fix incorrect file path in config.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 ENV_PATH = ".env"
 
 load_dotenv(BASE_DIR / ENV_PATH)
