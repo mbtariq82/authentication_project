bc90c57df3abdc541762a1affea93c02cc1309f3
TanushreeBante <tanushreebante@gmail.com>
2026-08-14 11:51:32 +0100

    added admin api and deleted document_id from accounts and aded user_status field

--- backend/router/admin.py
+++ backend/router/admin.py
@@ -1,16 +1,37 @@
-from fastapi import APIRouter, Depends
 
-from domain.user import User
-from schemas.user import UserResponse
+
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
 from dependencies.auth import require_admin
+from dependencies.database import get_db
+from schemas.admin_schema import UserStatusUpdate, UserResponse
+from services.admin_service import AdminUserService
 
-router = APIRouter(
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(require_admin)],
-)
+router = APIRouter(prefix="/admin", tags=["Admin"])
+# , dependencies=[Depends(require_admin)]
 
-@router.get("/dashboard")
-async def get_admin_dashboard() -> None:
-    return
-    # TODO: Query the actual data for the admin dashboard and return it in the response
+@router.patch("/users/{user_id}/status")
+async def update_user_status(
+    user_id: int,
+    data: UserStatusUpdate,
+    db: AsyncSession = Depends(get_db),
+):
+    service = AdminUserService(db)
+
+    return await service.update_status(
+        user_id=user_id,
+        new_status=data.status,
+    )
+
+@router.get("/all_users", response_model=list[UserResponse])
+async def get_all_users(
+    skip: int = Query(0, ge=0),
+    limit: int = Query(100, ge=1, le=100),
+    db: AsyncSession = Depends(get_db),
+):
+    service = AdminUserService(db)
+
+    return await service.get_all_users(
+        skip=skip,
+        limit=limit,
+    )
