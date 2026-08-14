cf6672bf29e3e3a7124bf9b69ea1cc094db21e1f
lumi-sola <oluwapelumi.aregbesola@informationtechconsultants.co.uk>
2026-08-14 12:31:02 +0100

    Split UoW into its own DI provider based on feedback

--- backend/dependencies/accounts.py
+++ backend/dependencies/accounts.py
@@ -1,10 +1,13 @@
+from fastapi import Depends
 from database import async_session_factory
 from services.account_service import AccountService
-from unit_of_work.sqlalchemy_account_unit_of_work import (
-    SqlAlchemyAccountUnitOfWork,
-)
+from unit_of_work.abstract_account_unit_of_work import AbstractAccountUnitOfWork
+from unit_of_work.sqlalchemy_account_unit_of_work import SqlAlchemyAccountUnitOfWork
 
+def get_account_unit_of_work() -> AbstractAccountUnitOfWork:
+    return SqlAlchemyAccountUnitOfWork(async_session_factory)
 
-def get_account_service() -> AccountService:
-    unit_of_work = SqlAlchemyAccountUnitOfWork(async_session_factory)
+def get_account_service(
+    unit_of_work: AbstractAccountUnitOfWork = Depends(get_account_unit_of_work),
+) -> AccountService:
     return AccountService(unit_of_work)
