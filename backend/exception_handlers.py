eedf48dfb111bc1d4add49c15e24c92a79cff1a5
Noah Amoo <yawnoah3n@yahoo.com>
2026-08-21 09:19:09 +0100

    Rectified the name errors for unit tests

--- backend/exception_handlers.py
+++ backend/exception_handlers.py
@@ -22,6 +22,7 @@ from exceptions import (
     InsufficientFundsError,
     InvalidTransactionRuleError,
     InvalidTransactionStatusTransitionError,
+    SelfTransferError,
     TransactionNotFoundError,
     PermissionDeniedError,
     ProfileImageStorageError,
